fe760ecb1ab38f012f70d73bea8ffb5ac61824f4
20A1 <ayshaowolabi@gmail.com>
2023-05-12 14:38:46 -0700

    fix

---
+++ models/state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A module containing the definition for the 'State' class which is a subclass of
+the 'BaseModel' class
+"""
+
+
+from .base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    A class representation of a state, this class inherits from the 'BaseModel'
+    class and has one additional class attribute 'name'
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init(*args, **kwargs)
+        if kwargs is None or len(kwargs) == 0:
+            self.name = ""
+        else:
+            if "name" in kwargs:
+                self.name = kwargs["name"]
+
+
+
