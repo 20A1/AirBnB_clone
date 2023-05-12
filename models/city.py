2c71aa530616c9a8dbe42b53b8a5ca0f47076c7d
20A1 <ayshaowolabi@gmail.com>
2023-05-12 14:44:08 -0700

    fix

---
+++ models/city.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module contains the definition for the 'City' class which is a subclass of
+the 'BaseModel' class
+"""
+
+
+from .base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    A class representation of a city, with attributes as follows:
+
+        - state_id: string - empty string: it will be the State.id
+        - name: string - empty string
+    """
+
+    def __init__(self, *args, **kwargs):
+        if kwargs is None or len(kwargs) == 0:
+            self.state_id = ""
+            self.name = ""
+        else:
+            if "state_id" in kwargs:
+                self.state_id = kwargs["state_id"]
+            else:
+                self.state_id = ""
+            if "name" in kwargs:
+                self.name = kwargs["name"]
+            else:
+                self.name = ""
+
