6c5ba4710979158d0098725b44053c8125b9256c
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-05-14 11:52:20 +0100

    Calls super().__init__() after initialization of classes attributes so all attributes can be saved by the save() call in super().__init__()

--- models/place.py
+++ models/place.py
@@ -27,7 +27,6 @@ class Place(BaseModel):
     """
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
         if kwargs is None or len(kwargs) == 0:
             self.city_id = ""
             self.user_id = ""
@@ -41,47 +40,9 @@ class Place(BaseModel):
             self.longitude = 0.0
             self.amenity_ids = []
         else:
-            if "city_id" in kwargs:
-                self.city_id = kwargs["city_id"]
-            else:
-                self.city_id = ""
-            if "user_id" in kwargs:
-                self.user_id = kwargs["user_id"]
-            else:
-                self.user_id = ""
-            if "name" in kwargs:
-                self.name = kwargs["name"]
-            else:
-                self.name = ""
-            if "description" in kwargs:
-                self.description = kwargs["description"]
-            else:
-                self.description = ""
-            if "number_rooms" in kwargs:
-                self.number_rooms = kwargs["number_rooms"]
-            else:
-                self.number_rooms = 0
-            if "number_bathrooms" in kwargs:
-                self.number_bathrooms = kwargs["number_bathrooms"]
-            else:
-                self.number_bathrooms = 0
-            if "max_guest" in kwargs:
-                self.max_guest = kwargs["max_guest"]
-            else:
-                self.max_guest = 0
-            if "price_by_night" in kwargs:
-                self.price_by_night = kwargs["price_by_night"]
-            else:
-                self.price_by_night = 0
-            if "latitude" in kwargs:
-                self.latitude = kwargs["latitude"]
-            else:
-                self.latitude = 0.0
-            if "longitude" in kwargs:
-                self.longitude = kwargs["longitude"]
-            else:
-                self.longitude = 0.0
-            if "amenity_ids" in kwargs:
-                self.amenity_ids = kwargs["amenity_ids"]
-            else:
-                self.amenity_ids = []
+            for key in kwargs:
+                if key == "__class__":
+                    continue
+                else:
+                    setattr(self, key, kwargs[key])
+        super().__init__(*args, **kwargs)
